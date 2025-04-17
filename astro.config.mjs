c526678f4493ca891ae8f75b1bdd36b51ac1d72e
ogeobsys <148046882+ogeobsys@users.noreply.github.com>
2025-04-17 17:25:00 +0200

    ogeobsys -> geobsys

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://ogeobsys.github.io',
+	site: 'https://geobsys.github.io',
   	base: '/docGeobsys',
 	integrations: [
 		starlight({

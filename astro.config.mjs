1174e6e2c153330b8da5481bc6e80799012fd3a2
ogeobsys <omartin@geobsys.com>
2025-04-17 15:07:18 +0200

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,24 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://ogeobsys.github.io',
+  	base: '/docGeobsys',
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'GEOBSYS Docs',
+			logo: {
+				light: './src/assets/docGeobsys2.svg',
+				dark: './src/assets/docGeobsysDark2.svg',
+				replacesTitle: true,
+			},
+			customCss: [
+				// Relative path to your custom CSS file
+				'./src/styles/custom.css',
+				'@fontsource/montserrat/100.css'
+			],
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{

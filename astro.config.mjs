b93b86acf3cfc7238c2128714211b18317ef46cc
ogeobsys <148046882+ogeobsys@users.noreply.github.com>
2025-04-24 21:34:37 +0200

    first proto

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,39 +1,116 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-
+import starlightSidebarTopics from 'starlight-sidebar-topics'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://geobsys.github.io',
-  	base: '/docGeobsys',
-	integrations: [
-		starlight({
-			title: 'GEOBSYS Docs',
-			logo: {
-				light: './src/assets/docGeobsys2.svg',
-				dark: './src/assets/docGeobsysDark2.svg',
-				replacesTitle: true,
-			},
-			customCss: [
-				// Relative path to your custom CSS file
-				'./src/styles/custom.css',
-				'@fontsource/montserrat/100.css'
-			],
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+    site: 'https://geobsys.github.io',
+  	base: process.env.NODE_ENV === 'production' ? '/docGeobsys' : '/',
+    integrations: [starlight({
+        title: 'GEOBSYS Docs',
+        // Set English as the default language for this site.
+        defaultLocale: 'en',
+        locales: {
+          // English docs in `src/content/docs/en/`
+          'en': {
+            label: 'English',
+            lang: 'en',
+          },
+          'fr': {
+            label: 'French',
+            lang: 'fr',
+          },
+        },
+        /*sidebar: [
+          {
+            label: 'Start Here',
+            items: [
+              // Using `slug` for internal links.
+              { slug: 'geostix/start' },
+              { slug: 'geostix-app/start' },
+              // Or using the shorthand for internal links.
+              'geostix/start',
+              'geostix-app/start',
+            ],
+          },
+          // A group linking to all pages in the reference directory.
+          {
+            label: 'geostix layers',
+            autogenerate: { directory: 'geostix-layers' },
+          },
+        ],*/
+        logo: {
+            light: './src/assets/docGeobsys2.svg',
+            dark: './src/assets/docGeobsysDark2.svg',
+            replacesTitle: true,
+        },
+        customCss: [
+            // Relative path to your custom CSS file
+            './src/styles/custom.css',
+            '@fontsource/montserrat/100.css'
+        ],
+        plugins: [
+          starlightSidebarTopics(
+            [
+              {
+                label: 'Geostix',
+                link: '/geostix/getting-started/',
+                icon: 'document',
+                items: [
+                  { label: 'Start Here', items: ['geostix/getting-started', 'geostix/configuration'] },
+                  { label: 'Guides', autogenerate: { directory: 'geostix/guides' } },
+                  { label: 'Resources', items: [{ label: 'Plugins and Tools', slug: 'geostix/resources/starlight' }] },
+                ],
+              },
+              {
+                label: 'Etages capteurs',
+                link: '/geostix-layers/getting-started/',
+                icon: 'document',
+                items: [
+                  { label: 'Start Here', items: ['geostix-layers/getting-started', 'geostix-layers/configuration'] },
+                  { label: 'Guides', autogenerate: { directory: 'geostix-layers/guides' } },
+                  { label: 'Resources', items: [{ label: 'Plugins and Tools', slug: 'geostix-layers/resources/starlight' }] },
+                ],
+                badge: {
+                  text: {
+                    en: 'Stub',
+                    fr: 'Ébauche',
+                  },
+                  variant: 'caution',
+                },
+              },
+              {
+                label: 'Geostix APP',
+                link: '/geostix-app/getting-started/',
+                icon: 'document',
+                items: [
+                  { label: 'Start Here', items: ['geostix-app/getting-started', 'geostix-app/configuration'] },
+                  { label: 'Guides', autogenerate: { directory: 'geostix-app/guides' } },
+                  { label: 'Resources', items: [{ label: 'Plugins and Tools', slug: 'geostix-app/resources/starlight' }] },
+                ],
+                badge: {
+                  text: {
+                    en: 'Stub',
+                    fr: 'Ébauche',
+                  },
+                  variant: 'caution',
+                },
+              },
+              {
+                label: 'GEOBSYS website',
+                link: 'https://geobsys.com',
+                icon: 'right-caret',
+              },
+            ],
+            {
+              //exclude: ['/demo/excluded/**/*', '/*/demo/excluded/**/*'],
+              //topics: {
+              //  demo: ['/demo/unlisted/topics', '/*/demo/unlisted/topics'],
+              //},
+            },
+          ),
+        ],
+        social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+		})],
 });
